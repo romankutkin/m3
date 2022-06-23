4acf2b2955d53015ed97f3ea925fa371937d3ed1
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-06-23 17:18:50 +0700

    Add composer install in docker-entrypoint

--- docker/etc/php-fpm/docker-entrypoint.sh
+++ docker/etc/php-fpm/docker-entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 if [ "$1" = 'php-fpm' ]; then
+    composer install --prefer-dist --no-progress --no-interaction
+
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
