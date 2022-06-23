9556aaf8fb9233e17827a59cdd0d84efa1352864
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-06-23 15:16:21 +0700

    Add docker entrypoint with setfacl

---
+++ docker/etc/php-fpm/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'php-fpm' ]; then
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+fi
+
+exec docker-php-entrypoint "$@"
