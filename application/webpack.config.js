8cbe36c6471806cad50bcbe3a3ebc3868654fbdb
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-08-22 17:01:51 +0700

    Add babel loader in webpack

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -5,6 +5,23 @@ module.exports = {
     entry: {
         index: './assets/js/index.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            },
+        ],
+    },
     output: {
         path: path.resolve(__dirname, 'public/bundles'),
         filename: '[name].bundle.js',
