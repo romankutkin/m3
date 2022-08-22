386de7f23c54987006cc1e445a19a8269cc61c97
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-08-22 17:16:08 +0700

    Add sass and sass-loader in webpack

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
                 use: [
                     "style-loader",
                     "css-loader",
+                    "sass-loader",
                 ],
             },
             {
