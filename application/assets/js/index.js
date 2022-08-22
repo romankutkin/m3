aa8b4542284ca3246f145b4a01787b052bb24166
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-08-22 17:02:14 +0700

    Create Greeting component

--- application/assets/js/index.js
+++ application/assets/js/index.js
@@ -1,1 +1,7 @@
-console.log('index.js');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Greeting from "./components/Greeting.jsx";
+
+const container = document.querySelector('#greeting');
+const root = createRoot(container);
+root.render(<Greeting />);
